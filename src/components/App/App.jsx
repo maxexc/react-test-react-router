bff17a2f8d7ba84d4ea4f4cf9e638a0c9958c34c
Maksym Solomko <max.exc7@gmail.com>
2023-01-06 00:56:46 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+import { About } from "pages/About";
+import { Home } from "pages/Home";
+import { SharedLayout } from "components/SharedLayout";
+import { Mission } from "components/Mission";
+import { Team } from "components/Team";
+import { Reviews } from "components/Reviews";
+import { Products } from "pages/Products";
+import { ProductDetails } from "pages/ProductDetails";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
